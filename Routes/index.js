9dcb8102f25c80f2083d87103ccae3f2a2fabb5c
farisdam <farisadam97@gmail.com>
2023-04-10 07:20:31 +0700

    import auth and account route on index route

--- Routes/index.js
+++ Routes/index.js
@@ -2,6 +2,8 @@ var express = require("express");
 var router = express.Router();
 
 const authRouter = require("./Auth.route");
+const accountRouter = require("./Account.route");
+const authMiddleware = require("../Middleware/Auth");
 
 /* GET home page. */
 router.get("/welcome", function (req, res, next) {
@@ -10,5 +12,8 @@ router.get("/welcome", function (req, res, next) {
 
 // Api route for authentication
 router.use("/auth", authRouter);
+
+// Api route for account
+router.use("/account", authMiddleware, accountRouter);
 
 module.exports = router;
