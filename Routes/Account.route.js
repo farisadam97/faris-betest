62bd5aac92309e4fa01e2f243c1a335480ef0754
faris <farisadam97@gmai.com>
2023-04-10 14:16:01 +0700

    add new route for account to get last login history

--- Routes/Account.route.js
+++ Routes/Account.route.js
@@ -3,6 +3,10 @@ const accountRouter = Router();
 
 const accountController = require("../Controller/Account.controller");
 
+accountRouter.route("/").get(accountController.getAllAccount);
+accountRouter
+  .route("/last-login-history")
+  .get(accountController.getLastLoginThreeDays);
 accountRouter.route("/:id").get(accountController.getAccountById);
 accountRouter.route("/:id").put(accountController.findAccountAndUpdate);
 accountRouter.route("/:id").delete(accountController.deleteAccount);
